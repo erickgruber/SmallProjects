b05e7d3cba375cfeec9088cc4332e2de59fc86d6
Erick Gruber <erickgruber@outlook.com>
2020-10-06 10:00:38 +0200

    Player

--- video-player/script.js
+++ video-player/script.js
@@ -1,16 +1,94 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-butn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('fullscreen');
 
 // Play & Pause ----------------------------------- //
+
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause-circle', 'fa-play-circle');
+    playBtn.setAttribute('title', 'Play');
+}
+
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+        playBtn.classList.replace('fa-play-circle', 'fa-pause-circle');
+        playBtn.setAttribute('title', 'Pause');
+    } else {
+        video.pause();
+        showPlayIcon();
+    }
+}
+
+// On Video End, show play Button icon
+video.addEventListener('ended', showPlayIcon);
 
 
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    // console.log(minutes, seconds);
+    return `${minutes}:${seconds}`;
+}
+
+// Update progress bar as video plays
+function updateProgress() {
+    // console.log('currentTime', video.currentTime, 'duration', video.duration);
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
+}
+
+// Click to seek within the video
+function setProgress(e) {
+    // console.log(e);
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+    // console.log(newTime);
+}
 
 
 // Volume Controls --------------------------- //
 
+// Volume Bar
+function changeVolume(e) {
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    // Rounding volue up or down
+    if (volume < 0.1) {
+        volume = 0;
+    }
 
+    if (volume > 0.9) {
+        volume = 1;
+    }
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+
+    // console.log(volume);
+
+    // Change Icon depending on volume
+    volumeIcon.className = '';
+    if (volume > 0.7) {
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+    } else if (volume < 0.7 && volume > 0) {
+        volumeIcon.classList.add('fas', 'fa-volume-down');
+    } else if (volume === 0) {
+        volumeIcon.classList.add('fas', 'fa-volume-off');
+    }
+}
 
 // Change Playback Speed -------------------- //
 
@@ -18,4 +96,10 @@
 
 // Fullscreen ------------------------------- //
 
-
+// Event Listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
